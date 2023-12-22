e0d5e081f520dabc684fb0fe780e51c96c48fa01
worktesting1 <worktesting099@gmail.com>
2023-12-22 15:37:00 +0100

    updated

--- src/components/DashboardHeader/DashboardHeader.jsx
+++ src/components/DashboardHeader/DashboardHeader.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import "./DashboardHeader.css";
-import LoginImageHero from "../../asset/profit.svg";
+import LoginImageHero from "../../asset/Logo.svg";
 import { RiMenu3Line } from "react-icons/ri";
 
 const DashboardHeader = ({ toggleVisibility }) => {
