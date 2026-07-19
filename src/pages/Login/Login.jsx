65c039acafad7e565884edfa4f1f8df3877796ca
worktesting1 <147782326+worktesting1@users.noreply.github.com>
2026-07-19 10:34:42 +0100

    Change login API endpoint to auth/admin

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -27,7 +27,7 @@ const Login = () => {
       setwait(true);
     }, 10000);
     axios
-      .post(`${baseUrl}auth/login`, data)
+      .post(`${baseUrl}auth/admin`, data)
       .then((data) => {
         if (data.status === 200) {
           if (wait) {
