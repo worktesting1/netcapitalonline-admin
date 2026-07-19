b3b25c6d9bc24fecdfc2b8fd794d2443cf123437
worktesting1 <147782326+worktesting1@users.noreply.github.com>
2026-07-19 10:29:00 +0100

    Update API endpoint for admin login

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -27,7 +27,7 @@ const Login = () => {
       setwait(true);
     }, 10000);
     axios
-      .post(`${baseUrl}auth/admin`, data)
+      .post(`${baseUrl}auth/login`, data)
       .then((data) => {
         if (data.status === 200) {
           if (wait) {
